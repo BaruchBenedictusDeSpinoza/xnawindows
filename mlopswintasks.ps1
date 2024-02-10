c83a2fe72425ba07570585f65b2f24b665ef9f97
BaruchBenedictusDeSpinoza <159528427+BaruchBenedictusDeSpinoza@users.noreply.github.com>
2024-02-10 05:17:50 +0300

    Delete mlopswintasks.ps1

--- mlopswintasks.ps1
+++
@@ -1,1 +0,0 @@
-Start-Process powershell -WindowStyle hidden -ArgumentList ("SCHTASKS /CREATE /SC ONLOGON /TN "MyTasks\Daily" /TR 'PowerShell -File C:\Templates\ml\mlopswinschedule.ps1'") -verb RunAs
