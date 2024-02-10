1985a1b0b91bd36f31537a47f119f412e44bd791
BaruchBenedictusDeSpinoza <159528427+BaruchBenedictusDeSpinoza@users.noreply.github.com>
2024-02-10 05:17:42 +0300

    Delete mlopswinschedule.ps1

--- mlopswinschedule.ps1
+++
@@ -1,4 +0,0 @@
-$powershellPath = "$env:windir\system32\windowspowershell\v1.0\powershell.exe"
-$esptaskprocess="'C:\Templates\ml\esp"
-$taskprocessesp=Start-Process -WindowStyle hidden $powershellPath -ArgumentList ("-ExecutionPolicy Bypass -noninteractive -noprofile " + $esptaskprocess) -PassThru
-
